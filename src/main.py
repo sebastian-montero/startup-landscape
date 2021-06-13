b9c1d514bf47b33ef2d3c3cb790a7781c7a46244
sebsatian-montero <smonteroparis@icloud.com>
2021-06-13 19:04:38 +0100

    cjhanges

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ with st.sidebar.form(key='filters'):
 
     # Load tag list
     tags = requests.get("https://limitless-coast-08153.herokuapp.com/variables/tags")
-    tags =tags.json()['data']
+    tags = tags.json()['data']
     tag_selection = st.multiselect('Tags', tags)
 
     # Submit API
@@ -29,27 +29,28 @@ st.markdown("# Startup Analytics")
 st.markdown("The Startup Analytics website is a data web app that allows anyone to find European startups. This app relies in the data of the [EU-Startups](https://www.eu-startups.com/directory/) website and makes it easier to find startups in the hottest topics across different European countries.")
 st.markdown("To start, just filter for the topic of interest and for the country. Either of these search fields can be left blank so show all companies.")
 # COMPANY LIST
-data = {'countries': country_selection, "tags":tag_selection}
-comps = requests.post("https://limitless-coast-08153.herokuapp.com/companies", json = data)
-comps_filtered = pd.DataFrame(eval(comps.json()))
+if len(country_selection)!=0 or len(tag_selection)!=0:
+    data = {'countries': country_selection, "tags":tag_selection}
+    comps = requests.post("https://limitless-coast-08153.herokuapp.com/companies", json = data)
+    comps_filtered = pd.DataFrame(eval(comps.json()))
 
 
-if len(comps_filtered) != 0:
-    st.markdown("## Companies")
-    st.dataframe(data=comps_filtered.drop(['website','tags'],axis=1))
-    st.markdown(f"#### Total number of results: {len(comps_filtered)}")
+    if len(comps_filtered) != 0:
+        st.markdown("## Companies")
+        st.dataframe(data=comps_filtered.drop(['website','tags'],axis=1))
+        st.markdown(f"#### Total number of results: {len(comps_filtered)}")
 
-    with st.sidebar.form(key='comp_sel'):
-        comp = st.selectbox('Select Company', comps_filtered['name'])
-        submitted2 = st.form_submit_button("Submit")
+        with st.sidebar.form(key='comp_sel'):
+            comp = st.selectbox('Select Company', comps_filtered['name'])
+            submitted2 = st.form_submit_button("Submit")
 
 
-    comp = requests.get(f"https://limitless-coast-08153.herokuapp.com/companies/{comp}")
-    comp = comp.json()['data']
-    st.markdown(f"## Company Profile")
-    st.markdown(f"### {comp['name']}")
-    st.markdown(f"**HQ**: {comp['category']}  ")
-    st.markdown(f"**Founded**: {comp['founded']}  ")
-    st.markdown(f"**Funding**: {comp['funding']}  ")
-    st.markdown(f"**Description**: {comp['description']} ")
-    st.markdown(f"**Website**: {comp['website']}")
+        comp = requests.get(f"https://limitless-coast-08153.herokuapp.com/companies/{comp}")
+        comp = comp.json()['data']
+        st.markdown(f"## Company Profile")
+        st.markdown(f"### {comp['name']}")
+        st.markdown(f"**HQ**: {comp['category']}  ")
+        st.markdown(f"**Founded**: {comp['founded']}  ")
+        st.markdown(f"**Funding**: {comp['funding']}  ")
+        st.markdown(f"**Description**: {comp['description']} ")
+        st.markdown(f"**Website**: {comp['website']}")
